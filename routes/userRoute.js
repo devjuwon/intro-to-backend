745966153b9532d7556d9ed2e92961e4fccf2ba7
devjuwon <devjuwonkuzoma@gmail.com>
2024-09-10 13:12:50 +0100

    updated backend

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,1 +1,23 @@
 // all user endpoints will be defined here
+const express = require("express")
+const { registerUser, registerAdmin } = require("../controllers/userController")
+
+const router = express.Router()
+
+// user route 
+router.post("/register", registerUser)
+
+
+// admin route
+router.post("/admin", registerAdmin)
+
+ 
+
+
+
+
+
+
+
+
+module.exports = router
